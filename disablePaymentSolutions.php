3de35b0e8e6554e3108d4ac7b43564087d66bc45
Oliver1269 <olivermarcuslarsen@gmail.com>
2025-08-27 15:00:39 +0200

    Basic setup and init functions

---
+++ disablePaymentSolutions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package disablePaymentSolutions
+ */
+/* 
+Plugin Name: Disable Payment Solutions
+Description: Disables certain payment solutions depending on a new product tag
+Version: 1.0.0
+Author: Oliver "Oliver1269" Larsen
+Author URI: https://github.com/Oliver1269ii
+License: GPL-3.0-or-later
+Text Domain: disablePaymentSolutions
+*/
+
+defined( 'ABSPATH' ) or die();
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+use Inc\base\Activate;
+register_activation_hook( __FILE__, 'Activate');
+function Activate() {
+    Activate::activate();
+}
+
+use Inc\base\Deactivate;
+register_deactivation_hook( __FILE__, 'Deactivate');
+function Deactivate() {
+    Deactivate::deactivate();
+}
+
+if ( class_exists( 'Inc\\Init' ) ) {
+	Inc\Init::register_services();
+}
---
+++ inc/base/Deactivate.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * @package disablePaymentSolutions
+ */
+
+namespace Inc\base;
+
+class Deactivate {
+    public static function deactivate(){
+        flush_rewrite_rules();
+    }
+}
